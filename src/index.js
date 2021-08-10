ad09632b90c9103a4af382a948879dbeaa19a1a1
sudharaD <sudharad7@gmail.com>
2021-08-10 08:41:30 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<script type="text/babel" data-presets="env,react">
+	const App = function () {
+		return(
+			<div>
+				<div className="ui placeholder segment">
+					<div className="ui icon header">
+						<i classname="pdf file outline icon"></i>
+						No documents are listed for this customer.
+					</div>
+					<div className="ui primary button">Add Document</div>
+				</div>
+				<div className="ui placeholder segment">
+					<h4 class="ui header">For Your Information</h4>
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+				</div>	
+			</div>
+		);
+    }
+</script>
